1f982ec215e6adc53d1034beb32369350b14800d
Jarrod Luca <jarrodluca@gmail.com>
2017-03-24 20:25:34 -0400

    practice files

---
+++ practice/functions.js
@@ -0,0 +1,10 @@
+// ==================== //
+//   FUNCTION PRACTICE  //
+// ==================== //
+
+// prints text
+function welcomeMsg() {
+  // var name = prompt("Enter your name: ");
+  console.log("Hello. Welcome!");
+}
+welcomeMsg();
